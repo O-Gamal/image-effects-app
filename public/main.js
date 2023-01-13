54c8b526544b2b57ba8103d623c4e1a7e9e53d84
omar <o.gamal.abdulzaher@gmail.com>
2023-01-13 09:58:39 +0200

    Minor updates

--- public/main.js
+++ public/main.js
@@ -13,15 +13,21 @@ async function init() {
   const input = document.getElementById('upload');
   const fileReader = new FileReader();
 
+  const imagesContainer = document.getElementById('imgs-container');
+  // const original = document.getElementById('original-img');
+  const img = document.getElementById('new-img');
+
   fileReader.onloadend = () => {
     let base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/,
       ''
     );
+
+    imagesContainer.classList.add('mt-8');
+    // original.src = fileReader.result;
+
     const img_data_url = rustApp.grayscale(base64);
-    const img = document.getElementById('new-img');
     img.src = img_data_url;
-    img.classList.add('mt-8');
   };
   input.addEventListener('change', () => {
     fileReader.readAsDataURL(input.files[0]);
